75b564e4fd4f91c8a48a073649a613266028c1a2
Rodrigo Queiroz <rodrigo.queiroz.chagas@gmail.com>
2018-06-07 23:22:37 -0300

    Adicionado seção SOBRE e PORTFOLIO

--- js/script.js
+++ js/script.js
@@ -1,15 +1,25 @@
-(function(){
-    var navbar =  document.querySelector(".navbar");
-    
+(function () {
     //Adiciona transparência na barra de navegação quando o usuário "scrola" a pagina um pouco para baixo
-    
+    var navbar = document.querySelector(".navbar");
+
     window.addEventListener("scroll", addTransparency);
-    
-    function addTransparency(){
-        if(scrollY > 50){
+
+    function addTransparency() {
+        if (scrollY > 50) {
             navbar.style.opacity = "0.9";
-        }else{
+        } else {
             navbar.style.opacity = "1";
         }
+    }
+    
+    //Alterna entre os projetos clicados
+    
+    var alert = document.querySelectorAll(".alert");
+    var project = document.querySelector(".project");
+    
+    for(var i = 0; i < alert.length; i++){
+        alert[i].addEventListener("click", function(){
+            project.textContent = this.textContent;
+        });
     }
 })();
