2f8e9803cdba22bbddd8a6bae1e6aaeb4b2e4eb1
Rodrigo Queiroz <rodrigo.queiroz.chagas@gmail.com>
2018-06-08 18:30:03 -0300

    Galeria de imagnes implementada

--- page-home.php
+++ page-home.php
@@ -1,73 +1,71 @@
-    <?php get_header();?>
-    <!---------------  SLIDER  --------------->
+<?php get_header();?>
+<!---------------  SLIDER  --------------->
 
-    <section id="top" class="slider">
-        <?php echo do_shortcode('[metaslider id="8"]'); ?>
-    </section>
+<section id="top" class="slider">
+    <?php echo do_shortcode('[metaslider id="8"]'); ?>
+</section>
 
-    <!---------------  ABOUT  --------------->
+<!---------------  ABOUT  --------------->
 
-    <section id="about" class="about">
-        <div class="container">
-            <h2 class="title">Sobre a <span class="text-uppercase">Tuut</span></h2>
-            <div class="alert alert-dark">
-                <p>A <span class="font-weight-bold text-uppercase">Tuut</span> conduz seu negócio com inteligência emocional, pensamento criativo e entusiasmo coletivo. Primado por uma comunicação integrada, nossa plataforma de atuação é múltipla e o nosso posicionamento e multidisciplinar. Aqui, a gente executa todo o projeto com estratégia, conceito, conteúdo e design. Raciocinamos em todas as mídias, viabilizando projetos para pequenas e grandes marcas, no Brasil e no exterior</p>
-            </div>
-        </div>
-    </section>
-
-    <!---------------  GALLERY  --------------->
+<section id="about" class="about">
     <div class="container">
-        <hr>
+        <h2 class="title">Sobre a <span class="text-uppercase">Tuut</span></h2>
+        <div class="alert alert-dark">
+            <p>A <span class="font-weight-bold text-uppercase">Tuut</span> conduz seu negócio com inteligência emocional, pensamento criativo e entusiasmo coletivo. Primado por uma comunicação integrada, nossa plataforma de atuação é múltipla e o nosso posicionamento e multidisciplinar. Aqui, a gente executa todo o projeto com estratégia, conceito, conteúdo e design. Raciocinamos em todas as mídias, viabilizando projetos para pequenas e grandes marcas, no Brasil e no exterior</p>
+        </div>
     </div>
+</section>
 
-    <section class="gallery">
+<!---------------  GALLERY  --------------->
+<div class="container">
+    <hr>
+</div>
+
+<section class="gallery">
+    <div class="container">
         <h2 class="title">Galeria <span class="text-uppercase">Tuut</span></h2>
-        <div>
-            <h2>Aqui vai a GALERIA :D!</h2>
-        </div>
-    </section>
-
-    <!---------------  GALLERY  --------------->
-    <div class="container">
-        <hr>
+        <?php echo do_shortcode('[supsystic-gallery id=1]') ?>
     </div>
+</section>
 
-    <section id="portfolio" class="portfolio">
-        <div class="container">
-            <h2 class="title">Alguns de nossos projetos</h2>
-            <div class="row">
-                <div class="col-md-6">
-                    <ul class="portfolio__ul">
-                        <div class="alert alert-dark" role="alert">Projeto 1</div>
-                        <div class="alert alert-darkness" role="alert">Projeto 2</div>
-                        <div class="alert alert-dark" role="alert">Projeto 3</div>
-                        <div class="alert alert-darkness" role="alert">Projeto 4</div>
-                        <div class="alert alert-dark" role="alert">Projeto 5</div>
-                        <div class="alert alert-darkness" role="alert">Projeto 6</div>
-                        <div class="alert alert-dark" role="alert">Projeto 7</div>
-                    </ul>
-                </div>
-                <div class="portfolio__desc col-md-6">
-                    <p>Nosso trabalho é feito em parceria com nossos clientes. Um trabalho a quatro mãos, em que nos envolvemos em todos os meandros do processo, propondo soluções práticas e acompanhando os resultados</p>
-                    <div class="project">Projeto 1</div>
-                </div>
+<!---------------  GALLERY  --------------->
+<div class="container">
+    <hr>
+</div>
+
+<section id="portfolio" class="portfolio">
+    <div class="container">
+        <h2 class="title">Alguns de nossos projetos</h2>
+        <div class="row">
+            <div class="col-md-6">
+                <ul class="portfolio__ul">
+                    <div class="alert alert-dark" role="alert">Projeto 1</div>
+                    <div class="alert alert-darkness" role="alert">Projeto 2</div>
+                    <div class="alert alert-dark" role="alert">Projeto 3</div>
+                    <div class="alert alert-darkness" role="alert">Projeto 4</div>
+                    <div class="alert alert-dark" role="alert">Projeto 5</div>
+                    <div class="alert alert-darkness" role="alert">Projeto 6</div>
+                    <div class="alert alert-dark" role="alert">Projeto 7</div>
+                </ul>
+            </div>
+            <div class="portfolio__desc col-md-6">
+                <p>Nosso trabalho é feito em parceria com nossos clientes. Um trabalho a quatro mãos, em que nos envolvemos em todos os meandros do processo, propondo soluções práticas e acompanhando os resultados</p>
+                <div class="project">Projeto 1</div>
             </div>
         </div>
-
-    </section>
-
-    <!---------------  GALLERY  --------------->
-    <div class="container">
-        <hr>
     </div>
 
+</section>
 
-    <section class="blog">
-        <h2>Aqui vai os 2 posts mais recentes</h2>
-    </section>
+<!---------------  GALLERY  --------------->
+<div class="container">
+    <hr>
+</div>
 
-    <!---------------  FOOTER  --------------->
-    <?php get_footer();?>
 
-    
+<section class="blog">
+    <h2>Aqui vai os 2 posts mais recentes</h2>
+</section>
+
+<!---------------  FOOTER  --------------->
+<?php get_footer();?>
